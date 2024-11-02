e795ba2cbbefe848317568b0e4061004899299ac
ngoc3324 <lengocanh3324@gmail.com>
2024-11-02 23:42:33 +0700

    update ui

--- WebApp/Models/Account/ChangePasswordModel.cs
+++ WebApp/Models/Account/ChangePasswordModel.cs
@@ -5,18 +5,18 @@ namespace WebApp.Models;
 public class ChangePasswordModel
 {
     [Required]
-    [Display(Name = "Current password")]
+    [Display(Name = "Mật khẩu hiện tại")]
     [DataType(DataType.Password)]
     public string OldPassword { get; set; }
     
     [Required]
-    [Display(Name = "New password")]
+    [Display(Name = "Mật khẩu mới")]
     [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
     [DataType(DataType.Password)]
     public string NewPassword { get; set; }
     
     [Required]
-    [Display(Name = "Comfirm new password")]
+    [Display(Name = "Xác nhận mật khẩu")]
     [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
     [Compare("NewPassword", ErrorMessage = "The password and confirmation password do not match.")]
     [DataType(DataType.Password)]
