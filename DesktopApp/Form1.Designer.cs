b5ff1f9871460233e50d947f7287ba749c0fd3d5
Khanh Nguyen <97591523+khanhdew@users.noreply.github.com>
2024-11-09 01:19:43 +0700

    update

--- DesktopApp/Form1.Designer.cs
+++ DesktopApp/Form1.Designer.cs
@@ -29,10 +29,16 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        SuspendLayout();
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(800, 450);
+        Name = "Form1";
+        Text = "Form1";
+        ResumeLayout(false);
     }
 
     #endregion
