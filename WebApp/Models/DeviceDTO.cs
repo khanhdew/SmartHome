b7ebeb9deaafb5a6fca641bbfb63473a1a24522c
Khanh Nguyen <khanhdeptrai292004@gmail.com>
2025-10-05 21:48:36 +0700

    add: file

---
+++ WebApp/Models/DeviceDTO.cs
@@ -0,0 +1,10 @@
+namespace WebApp.Models
+{
+    public class DeviceDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UserId { get; set; }
+    }
+}
+
